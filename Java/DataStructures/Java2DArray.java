2926a9ebe777b948d1212695f436dc5d48ee84d3
Ngô Tôn <ngoton007@yahoo.com>
2021-09-09 16:05:38 +0700

    Create Java2DArray.java

---
+++ Java/DataStructures/Java2DArray.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, 6).forEach(i -> {
+            try {
+                arr.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        
+        System.out.println(hourGlassSum(arr));
+    }
+    
+    public static int hourGlassSum(List<List<Integer>> arr) {
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (i > 1 && j > 1) {
+                    int testSum = arr.get(i-2).get(j-2) + 
+                    arr.get(i-2).get(j-1) + 
+                    arr.get(i-2).get(j) +
+                    arr.get(i-1).get(j-1) + 
+                    arr.get(i).get(j-2) + 
+                    arr.get(i).get(j-1) + 
+                    arr.get(i).get(j);
+                    if (testSum > maxSum) {
+                        maxSum = testSum;
+                    }
+                }
+            }
+        }
+        return maxSum;
+    }
+}
