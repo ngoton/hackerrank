b0592624e16379e13be16a4abfb3a662c9ee3a4d
Ngô Tôn <ngoton007@yahoo.com>
2021-09-09 16:20:06 +0700

    Create Java1DArray(Part2).java

---
+++ Java/DataStructures/Java1DArray(Part2).java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Solution {
+
+    public static boolean canWin(int leap, int[] game) {
+        return isWin(leap, game, 0);
+    }
+    
+    private static boolean isWin(int m, int[] arr, int i) {
+        if (i < 0 || arr[i] == 1) {
+            return false;
+        }
+        if ((i == arr.length - 1) || i + m > arr.length - 1) {
+            return true;
+        }
+
+        arr[i] = 1;
+        return isWin(m, arr, i + 1) || isWin(m, arr, i - 1) || isWin(m, arr, i + m);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int q = scan.nextInt();
+        while (q-- > 0) {
+            int n = scan.nextInt();
+            int leap = scan.nextInt();
+            
+            int[] game = new int[n];
+            for (int i = 0; i < n; i++) {
+                game[i] = scan.nextInt();
+            }
+
+            System.out.println( (canWin(leap, game)) ? "YES" : "NO" );
+        }
+        scan.close();
+    }
+}
