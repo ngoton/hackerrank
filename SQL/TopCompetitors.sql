9aa7711412a7a265a918c7d918333c0196853d76
Ngô Tôn <ngoton007@yahoo.com>
2021-09-21 15:53:34 +0700

    Create TopCompetitors.sql

---
+++ SQL/TopCompetitors.sql
@@ -0,0 +1,10 @@
+SELECT h.hacker_id, h.name
+FROM Submissions s
+JOIN Challenges c ON s.challenge_id = c.challenge_id
+JOIN Difficulty d ON c.difficulty_level = d.difficulty_level 
+JOIN Hackers h ON s.hacker_id = h.hacker_id
+WHERE s.score = d.score 
+    AND c.difficulty_level = d.difficulty_level
+GROUP BY h.hacker_id, h.name
+HAVING COUNT(s.hacker_id) > 1
+ORDER BY COUNT(s.hacker_id) DESC, s.hacker_id ASC
